737ca82169206a9498da54c5bb7aff927f9d73c9
Ben Novikov <bigopendatanet@gmail.com>
2017-12-04 05:47:27 +0200

    @PostProxy custom annotation made with ContextListener pattern

---
+++ src/main/java/quoters/PostProxy.java
@@ -0,0 +1,11 @@
+package quoters;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/*
+ * @author Ben Novikov on December 2017
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PostProxy {
+}
---
+++ src/main/java/quoters/PostProxyInvokerContextListener.java
@@ -0,0 +1,47 @@
+package quoters;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.io.ClassPathResource;
+
+import java.lang.reflect.Method;
+
+/*
+ * @author Ben Novikov on December 2017
+ */
+public class PostProxyInvokerContextListener implements ApplicationListener<ContextRefreshedEvent> {
+    private final ConfigurableListableBeanFactory factory;
+
+    @Autowired
+    public PostProxyInvokerContextListener() {
+        this.factory = new XmlBeanFactory(new ClassPathResource("context.xml"));
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        ApplicationContext context = event.getApplicationContext();
+        String[] names = context.getBeanDefinitionNames();
+        for (String name : names) {
+            BeanDefinition beanDefinition = factory.getBeanDefinition(name);
+            String originalClassName = beanDefinition.getBeanClassName();
+            try {
+                Class<?> originalClass = Class.forName(originalClassName);
+                Method[] methods = originalClass.getMethods();
+                for (Method method : methods) {
+                    if (method.isAnnotationPresent(PostProxy.class)) {
+                        Object bean = context.getBean(name);
+                        Method currentMethod = bean.getClass().getMethod(method.getName(), method.getParameterTypes());
+                        currentMethod.invoke(bean);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
