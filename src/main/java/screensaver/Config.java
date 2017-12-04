cad4c36d658b1f950b61c6d8b21c1ea12e8bf37e
Ben Novikov <bigopendatanet@gmail.com>
2017-12-04 22:16:19 +0200

    Config defines colorFrame() bean @Scope("prototype")

---
+++ src/main/java/screensaver/Config.java
@@ -0,0 +1,43 @@
+package screensaver;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+
+import java.awt.*;
+import java.util.Random;
+
+/*
+ * @author Ben Novikov on December 2017
+ */
+@Configuration
+@ComponentScan(basePackages = "screensaver")
+public class Config {
+    @Bean
+    @Scope("prototype")
+    public Color color() {
+        Random random = new Random();
+        return new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
+    }
+
+    @Bean
+    public ColorFrame colorFrame() {
+        return new ColorFrame() {
+            @Override
+            protected Color getColor() {
+                return color();
+            }
+        };
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        while (true) {
+            context.getBean(ColorFrame.class).showOnRandomPlace();
+            Thread.sleep(100);
+        }
+    }
+}
