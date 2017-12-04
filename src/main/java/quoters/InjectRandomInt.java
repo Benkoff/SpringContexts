72655c6a6208de3311f73f8fe0a4ad0db214af02
Ben Novikov <bigopendatanet@gmail.com>
2017-12-04 02:14:03 +0200

    @InjectRandomInt annotation added

---
+++ src/main/java/quoters/InjectRandomInt.java
@@ -0,0 +1,13 @@
+package quoters;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/*
+ * @author Ben Novikov on December 2017
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface InjectRandomInt {
+    int min();
+    int max();
+}
--- src/main/java/quoters/TerminatorQuoter.java
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -7,6 +7,8 @@ import lombok.Data;
  */
 @Data
 public class TerminatorQuoter implements Quoter {
+    @InjectRandomInt(min = 2, max = 7)
+    private int repeat;
     private String message;
 
     public void setMessage(String message) {
@@ -15,6 +17,8 @@ public class TerminatorQuoter implements Quoter {
 
     @Override
     public void sayQuote() {
-        System.out.println("message = " + message);
+        for (int i = 0; i < repeat; i++) {
+            System.out.println("message = " + message);
+        }
     }
 }
